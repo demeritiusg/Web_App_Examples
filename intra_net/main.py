cf986103287cca556b3c6ea18948ef61de336f5f
Admin <demeritiusgriffin@gmail.com>
2020-06-19 18:59:40 -0500

    added logging

--- intra_net/main.py
+++ intra_net/main.py
@@ -5,11 +5,12 @@ import aiohttp_jinja2
 from settings import config
 from routes import setup_routes, setup_static
 from middlewares import setup_middlewares
-
+import logging
 
 def web_server(loop):
 
     app = web.Application(loop=loop)
+
     app['config'] = config
 
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('./templates'))
@@ -30,4 +31,5 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.call_later(0.1, wakeup)
     server = web_server(loop)
+    logging.basicConfig(level=logging.DEBUG)
     web.run_app(server, host='127.0.0.1', port=8080)
