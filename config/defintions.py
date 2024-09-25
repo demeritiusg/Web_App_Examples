dbfb578dfbf2d7a7c00e35bc34d494cf81bb9a6c
demeritiusg <demeritiusgriffin@gmail.com>
2024-09-24 17:01:04 -0700

    new files

---
+++ config/defintions.py
@@ -0,0 +1,58 @@
+QBO_REVENUE_ACCOUNTS = {
+    'Oil Interest': '4000',
+    'Oil Interest': '4010',
+    'Gas Interest': '4100',
+    'Gas Interest': '4110',
+    'Product': '4210',
+    'Miscellaneous': '4400'
+}
+
+QBO_EXPENSE_ACCOUNTS = {
+    'Production': '5000',
+    'Production': '5001',
+    'State Backup W/H - NM': '5010',
+    'State Backup W/H - OK': '5015',
+    'State Backup W/H - CA': '5020',
+    'State Backup W/H - MT': '5025',
+    'State Backup W/H - ND': '5030',
+    'State Backup W/H - UT': '5035',
+    'State Backup W/H - WY': '5040',
+    'State Backup W/H - CO': '5045',
+    'State Backup W/H - PA': '5050',
+    'State Withholding Tax - Unallocated': '5091',
+    'Other Oil & Gas Expenses:Marketing Expense (W)': '5120',
+    'Other Oil & Gas Expenses:Marketing Expense (R)': '5121',
+    'Other Oil & Gas Expenses:Other Expense - Royalty': '5331',
+    'Other Oil & Gas Expenses:Reimbursements to Prior Owners': '5350',
+    'Other Oil & Gas Expenses:Lease Operating Expense': '5400',
+    'Lease Operating Expense': '5400'
+}
+
+QBO_COMPANY_DOMAINS = {
+    'WIRC': 'https://c2.qbo.intuit.com',
+    'CYPT': 'https://c3.qbo.intuit.com',
+    'LTD': 'https://c3.qbo.intuit.com',
+    'LLC': 'https://c3.qbo.intuit.com',
+    'RCP': 'https://c3.qbo.intuit.com',
+    'MILL': 'https://c3.qbo.intuit.com',
+    'FOOT': 'https://c6.qbo.intuit.com',
+    'SUN': 'https://c71.qbo.intuit.com',
+    'RSM': 'https://c43.qbo.intuit.com',
+    'SHR': 'https://c41.qbo.intuit.com',
+}
+
+QBO_COMPANIES = {
+    'production':
+        ['RCM', 'RCP', 'CYPT', 'FOOT', 'LLC', 'LTD',
+         'MILL', 'WIRC', 'MIRH', 'SUN', 'RSM', 'SHR']}
+
+
+redirect_uri = "https://call-back"
+
+db_username = []
+db_pass = []
+db_uri = []
+
+sugar_username = []
+sugar_key = []
+
