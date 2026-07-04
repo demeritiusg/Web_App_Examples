900b49ae499f5c47a353b9abd6baaca3e4771dec
demeritiusg <demeritiusgriffin@gmail.com>
2026-07-03 17:02:04 -0700

    removed old files

--- quickbooks_app/database/db_setup.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy
-
-class User(db.Model):
-    id = db.Column(db.Inter)
--- quickbooks_app/intra_net/__init__.py
+++
@@ -1,5 +0,0 @@
-# TODO add the missing entity labels to the PC app
-# TODO need the unicorn image
-# TODO need the RC favcon
-# TODO need the RC logo
-# TODO add the header links
